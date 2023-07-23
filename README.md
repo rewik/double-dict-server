93a779116fbc74774244620a104a0757dc30768d
Karol Szumski <karol.szumski@gmail.com>
2023-07-23 18:15:34 +0200

    (+) First (non-usable) endpoint code

---
+++ README.md
@@ -0,0 +1,11 @@
+double-dict-server
+==================
+
+A very simple double-dict storage.
+
+Runs on 0.0.0.0:9000
+
+endpoints
+=========
+/api/health: GET, returns text "OK"
+/api/version: GET, returns text with the current version
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use axum::{
     routing,
-    Router
+    Router,
+    response::{Response, IntoResponse},
+    extract::{Path, State, RawBody},
 };
 use std::sync::Arc;
 use std::collections::HashMap;
 use tokio::sync::RwLock;
+use axum::http::status::StatusCode;
 
 struct AppState {
     data: [RwLock<HashMap<String, HashMap<String, Vec<u8>>>>; 256],
@@ -19,10 +22,35 @@ async fn main() {
     let app = Router::new()
         .route("/api/health", routing::get(|| async { "OK" }))
         .route("/api/version", routing::get(|| async { "0.1.0" }))
+        .route("/api/post/one/:primary/:secondary", routing::post(api_post_one))
+        .route("/api/get/one/:primary/:secondary", routing::get(api_get_one))
+        .route("/api/get/all/:primary", routing::get(api_get_all))
+        .route("/api/delete/one/:primary/:secondary", routing::delete(api_del_one))
+        .route("/api/delete/all/:primary", routing::delete(api_del_all))
         .with_state(state);
 
     axum::Server::bind(&"0.0.0.0:9000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+async fn api_post_one(State(state): State<Arc<AppState>>, Path(primary): Path<String>, RawBody(body): RawBody ) -> Response {
+    (StatusCode::NOT_FOUND).into_response()
+}
+
+async fn api_get_one(State(state): State<Arc<AppState>>, Path((primary, secondary)): Path<(String, String)>) -> Response {
+    (StatusCode::NOT_FOUND).into_response()
+}
+
+async fn api_get_all(State(state): State<Arc<AppState>>, Path(primary): Path<String>) -> Response {
+    (StatusCode::NOT_FOUND).into_response()
+}
+
+async fn api_del_one(State(state): State<Arc<AppState>>, Path((primary, secondary)): Path<(String, String)>) -> Response {
+    (StatusCode::NOT_FOUND).into_response()
+}
+
+async fn api_del_all(State(state): State<Arc<AppState>>, Path(primary): Path<String>) -> Response {
+    (StatusCode::NOT_FOUND).into_response()
 }
