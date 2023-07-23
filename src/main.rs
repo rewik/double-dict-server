4fdf5b17e0b6c29c1645a82efe67997483f9610c
Karol Szumski <karol.szumski@gmail.com>
2023-07-23 18:52:13 +0200

    (+) Implementation of most of the API endpoints (post one, get one,     delete one, delete all)

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
+
+use std::sync::Arc;
+use std::collections::HashMap;
+
 use axum::{
     routing,
     Router,
     response::{Response, IntoResponse},
-    extract::{Path, State, RawBody},
+    extract::{Path, State},
+    http::status::StatusCode,
+    body::Bytes,
 };
-use std::sync::Arc;
-use std::collections::HashMap;
 use tokio::sync::RwLock;
-use axum::http::status::StatusCode;
+use blake2::Digest;
 
 struct AppState {
     data: [RwLock<HashMap<String, HashMap<String, Vec<u8>>>>; 256],
@@ -35,22 +39,77 @@ async fn main() {
         .unwrap();
 }
 
-async fn api_post_one(State(state): State<Arc<AppState>>, Path(primary): Path<String>, RawBody(body): RawBody ) -> Response {
-    (StatusCode::NOT_FOUND).into_response()
+async fn api_post_one(State(state): State<Arc<AppState>>, Path((primary,secondary)): Path<(String,String)>, body: Bytes ) -> Response {
+    let bin = bin_from_string(&primary) as usize;
+    {
+        let mut rwl = state.data[bin].write().await;
+        let pk;
+        if let Some(x) = rwl.get_mut(&primary) {
+            pk = x;
+        } else {
+            rwl.insert(primary.clone(), HashMap::new());
+            pk = rwl.get_mut(&primary).unwrap();
+        }
+        pk.insert(secondary, body.to_vec());
+    }
+    (StatusCode::OK).into_response()
 }
 
 async fn api_get_one(State(state): State<Arc<AppState>>, Path((primary, secondary)): Path<(String, String)>) -> Response {
-    (StatusCode::NOT_FOUND).into_response()
+    let bin = bin_from_string(&primary) as usize;
+    {
+        let rwl = state.data[bin].read().await;
+        let Some(pk) = rwl.get(&primary) else {
+            return (StatusCode::NOT_FOUND).into_response();
+        };
+        let Some(data) = pk.get(&secondary) else {
+            return (StatusCode::NOT_FOUND).into_response();
+        };
+        (StatusCode::NOT_FOUND, data.clone()).into_response()
+    }
 }
 
 async fn api_get_all(State(state): State<Arc<AppState>>, Path(primary): Path<String>) -> Response {
+    let bin = bin_from_string(&primary) as usize;
     (StatusCode::NOT_FOUND).into_response()
 }
 
 async fn api_del_one(State(state): State<Arc<AppState>>, Path((primary, secondary)): Path<(String, String)>) -> Response {
-    (StatusCode::NOT_FOUND).into_response()
+    let bin = bin_from_string(&primary) as usize;
+    {
+        let mut rwl = state.data[bin].write().await;
+        {
+            let Some(pk) = rwl.get_mut(&primary) else {
+                return (StatusCode::NOT_FOUND).into_response();
+            };
+            let Some(_) = pk.remove(&secondary) else {
+                return (StatusCode::NOT_FOUND).into_response();
+            };
+            if !pk.is_empty() {
+                return (StatusCode::OK).into_response();
+            }
+        }
+        let _ = rwl.remove(&primary);
+    }
+    (StatusCode::OK).into_response()
 }
 
 async fn api_del_all(State(state): State<Arc<AppState>>, Path(primary): Path<String>) -> Response {
-    (StatusCode::NOT_FOUND).into_response()
+    let bin = bin_from_string(&primary) as usize;
+    {
+        let mut rwl = state.data[bin].write().await;
+        {
+            let Some(_) = rwl.remove(&primary) else {
+                return (StatusCode::NOT_FOUND).into_response();
+            };
+        }
+    }
+    (StatusCode::OK).into_response()
+}
+
+fn bin_from_string(code: &str) -> u8 {
+    let mut hasher = blake2::Blake2b512::new();
+    hasher.update(code.as_bytes());
+    let result = hasher.finalize();
+    result[0]
 }
